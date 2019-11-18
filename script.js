2d544ed57f98f6aad49d0a7414af612225e6b160
Michael Robil <ch.michaelsamir@hotmail.com>
2019-11-18 13:13:15 -0600

    initial api call

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
-$(function(){
+$(function() {
 
     var movies = [];
     var apiKey = "cff6c897";
     var title = 'rambo';
+    var movieTitle;
+    var moviePoster;
+    var movieYear;
+    var movieRating;
+    var movieDirector;
 
-    var queryURL ="http://www.omdbapi.com/?apikey=" + apiKey + "&t=" + title;
+    var queryURL = "http://www.omdbapi.com/?apikey=" + apiKey + "&t=" + title;
 
     $.ajax({
         url: queryURL,
         method: "GET"
-    }).then(function(response){
+    }).then(function(response) {
         console.log(response)
+        movieTitle = response.Title;
+        moviePoster = response.Poster;
+        movieYear = response.Year;
+        movieRating = response.Ratings[0];
+        movieDirector = response.Director;
+        console.log(movieTitle);
+
+
     });
 
 })
