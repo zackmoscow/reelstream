6918b431de30720299c7c8ced12dff2d9feccda1
Nicholas Cox <nick.a.cox@gmail.com>
2019-11-19 11:32:37 -0600

    moving things into the same file

--- youTubeAJAX.js
+++
@@ -1,8 +0,0 @@
-var movieTitle = 'rocky';
-var youTubeApiKey = "AIzaSyBcMUj0c7zvG1jkdogJmJgb94HkcMk_U-U";
-$.ajax({
-    url: `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${movieTitle}trailer&key=${youTubeApiKey}`,
-    method: "GET"
-}).then(function(response) {
-    console.log(response)
-})
