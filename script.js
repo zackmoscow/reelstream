8585ed092dbe945397cee9d09eb5823a1e98c593
Nicholas Cox <nick.a.cox@gmail.com>
2019-11-21 13:51:44 -0600

    tweaks

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-$(function() {
+$(function () {
     $('.parallax').parallax();
     var movies = [];
     var searchValue;
     var movieApiKey = "cff6c897";
-    var youTubeApiKey = "AIzaSyBcMUj0c7zvG1jkdogJmJgb94HkcMk_U-U";
+    var youTubeApiKey = "AIzaSyC5ImoUwTkX38aX8eAlbEW2_FP6fbLAVH0";
     var title;
     var movieTitle;
     var movieID;
@@ -19,7 +19,8 @@ $(function() {
 
     $('#results').hide();
 
-    searchBtn.click(function(e) {
+    searchBtn.click(function (e) {
+        //document.querySelector('#youTubeContainer').scrollIntoView();
         searchValue = $("#search").val().trim();
         title = searchValue;
         //console.log(searchValue);
@@ -28,7 +29,7 @@ $(function() {
         omdbAjaxRequest();
         $('#results').show();
     });
-    $('#search').on("keydown",function(e) {
+    $('#search').on("keydown", function (e) {
         if (e.keyCode === 13) {
             searchValue = $("#search").val().trim();
             title = searchValue;
@@ -39,7 +40,7 @@ $(function() {
             $('#results').show();
         }
     });
-    $(document).on("click", '.carousel-item', function(event){
+    $(document).on("click", '.carousel-item', function (event) {
         // event.preventDefault();
         searchValue = event.target.getAttribute('data-name');
         title = searchValue;
@@ -50,12 +51,12 @@ $(function() {
     })
     // hide the results box
     $('#results').hide;
-        // youtube ajax request. returns an array of 25 results. we're searching by title and year
+    // youtube ajax request. returns an array of 25 results. we're searching by title and year
     function youTubeAjaxRequest() {
         $.ajax({
             url: `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${movieTitle}${movieYear}trailer&key=${youTubeApiKey}`,
             method: "GET"
-        }).then(function(response) {
+        }).then(function (response) {
             videoID = response.items[0].id.videoId;
             var iframe = $(`<div class="video-container"><iframe width="560" height="315" src="https://www.youtube.com/embed/${videoID}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>`);
             $('#youTubeContainer').empty();
@@ -74,10 +75,11 @@ $(function() {
                 "x-rapidapi-key": "6036e8ca99msh30f244ef86faf3ep1e56f1jsn9c9368cad67e"
             }
         }
-        $.ajax(settings).done(function(response) {
+        $.ajax(settings).done(function (response) {
             var locations = response.results[0].locations;
             $('#utellyResult').empty();
-            locations.forEach(function(item) {
+            console.log(response);
+            locations.forEach(function (item) {
                 movieSource = item.name;
                 movieSourceUrl = item.url;
                 var span = $(`<span><a href="${movieSourceUrl}">${movieSource}</a></span>`);
@@ -91,7 +93,7 @@ $(function() {
         $.ajax({
             url: `https://api.themoviedb.org/3/movie/${movieID}/similar?api_key=34c4275ec69762284d8e87bcc5f7e573&language=en-US&page=1`,
             method: "GET"
-        }).then(function(response) {
+        }).then(function (response) {
             $('.carousel').empty();
             console.log(response);
             for (var i = 0; i < 19; i++) {
@@ -107,11 +109,11 @@ $(function() {
     var reccomendations;
     // omdb ajax request by title. the .then function runs the youtube request also.
     function omdbAjaxRequest() {
-        var queryURL = "http://www.omdbapi.com/?apikey=" + movieApiKey + "&t=" + title.replace('&', '%26');
+        var queryURL = "https://www.omdbapi.com/?apikey=" + movieApiKey + "&t=" + title.replace('&', '%26');
         $.ajax({
             url: queryURL,
             method: "GET"
-        }).then(function(response) {
+        }).then(function (response) {
             movieTitle = response.Title;
             moviePoster = response.Poster;
             movieYear = response.Year;
